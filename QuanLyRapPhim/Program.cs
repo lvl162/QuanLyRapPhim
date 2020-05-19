3da36c84f44857e7b4e07d1239e2f95c90558b99
Your Name <you@example.com>
2020-05-19 22:14:14 +0700

    test

--- QuanLyRapPhim/Program.cs
+++ QuanLyRapPhim/Program.cs
@@ -14,6 +14,7 @@ namespace QuanLyRapPhim
         [STAThread]
         static void Main()
         {
+            //TTTTessttt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
