7fd83b38b87c0a37a6c38f548340d9da396e2004
lvl162 <loithui162@gmail.com>
2020-05-18 22:09:25 -0700

    Test

--- QuanLyRapPhim/Program.cs
+++ QuanLyRapPhim/Program.cs
@@ -14,6 +14,7 @@ namespace QuanLyRapPhim
         [STAThread]
         static void Main()
         {
+            int a = 2;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
