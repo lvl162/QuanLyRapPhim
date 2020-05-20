afcea9a0e67b96f529c22858ced8914cd740b8dc
Your Name <you@example.com>
2020-05-20 16:55:39 +0700

    new form

--- QuanLyRapPhim/Form1.Designer.cs
+++ QuanLyRapPhim/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Đăng nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 45);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "QL Nhân viên";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(405, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 45);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "QL Bán Vé";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(583, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 45);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "QL Phim";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(55, 137);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 45);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "QL Lịch chiếu";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(583, 137);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 45);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "QL Phong";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(250, 145);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(236, 36);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Chọn ghế";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- QuanLyRapPhim/Form1.cs
+++ QuanLyRapPhim/Form1.cs
@@ -16,5 +16,40 @@ namespace QuanLyRapPhim
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new LoginForm().ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new QuanLyNhanVienForm().ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new QuanLyBanVe().ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            new QuanLyPhim().ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            new QuanLyLichChieuForm().ShowDialog();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            new SeatBookingForm().ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            new QuanLyPhongChieu().ShowDialog();
+        }
     }
 }
